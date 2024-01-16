2eceba774caca7750a4de62a41a443bb1ac4ca8f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-16 14:50:05 +0100

    Feat: Added landing page assets

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,21 @@ import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
-    <section>
+    <section className="w-full -h-full">
       <Navbar />
+
+      <section className="container">
+        <section className="hero">
+          
+          <section className="text">
+
+          </section>
+
+          <section className="image">
+            
+          </section>
+          </section>
+      </section>
     </section>
   );
 }
