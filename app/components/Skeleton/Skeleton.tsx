b851ac41e1c8f1c285bc8bbbd14c5ad55c1e872f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-16 12:38:48 +0100

    Feat: Generated app components with methane

---
+++ app/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from "react";
+
+interface IndexProps {
+    
+}
+
+const Index = ()=> {
+
+    return (
+        <div>
+            <h1>Index works!</h1>
+        </div>
+    );  
+}
+
+export default Index;
+    
