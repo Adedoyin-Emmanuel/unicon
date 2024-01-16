104fabf6e608905d932f6c930de9b2b6215faf4e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-16 16:27:02 +0100

    Feat: Finished login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,70 @@
-
-"use client"
+"use client";
 import React from "react";
 import { useRouter, usePathname } from "next/navigation";
+import Link from "next/link";
+import Text from "./../../components/Text/Text";
+import Input from "./../../components/Input/Input";
+import Button from "./../../components/Button/Button";
 
+const Login = () => {
+  const pathname = usePathname();
 
-const Index = () => {
-    const pathname = usePathname();
+  return (
+    <section className="w-screen h-screen flex items-center justify-center">
+      <form className="w-11/12 md:w-1/2 xl:w-1/4">
+        <section className="header-section my-8">
+          <h3 className="text-3xl capitalize font-bold text-secondary">
+            Welcome back
+          </h3>
+          <Text>organize, join, network, collaborate</Text>
+        </section>
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
+        <section className="my-4 mb-5">
+          <label htmlFor="email" className="text-md block my-2">
+            Email
+          </label>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Enter your email or username"
+          />
+        </section>
 
-export default Index;
-    
+        <section className="my-4 mb-5">
+          <label htmlFor="email" className="text-md block my-2">
+            Password
+          </label>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Shhhhh, it's a secret"
+          />
+        </section>
+
+        <section className="my-4 mb-5 w-full">
+          <Button>Login</Button>
+        </section>
+
+        <section>
+          <Text className="inline">
+            no account?
+            <Link
+              className="capitalize text-secondary px-1"
+              href={"/auth/signup"}
+            >
+              create account
+            </Link>
+            <Link
+              className="capitalize text-secondary px-1"
+              href={"/auth/forgot-password"}
+            >
+              Forgot Password
+            </Link>
+          </Text>
+        </section>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
