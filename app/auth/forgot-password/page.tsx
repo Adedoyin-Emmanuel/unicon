c817a749a8c6f75bd820e29fde28b08a78cdfb04
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-16 16:31:43 +0100

    Feat: Finished forgot-password page

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,19 +1,49 @@
-
-"use client"
+"use client";
 import React from "react";
 import { useRouter, usePathname } from "next/navigation";
+import Link from "next/link";
+import Text from "./../../components/Text/Text";
+import Input from "./../../components/Input/Input";
+import Button from "./../../components/Button/Button";
 
+const ForgotPassword = () => {
+  const pathname = usePathname();
 
-const Index = () => {
-    const pathname = usePathname();
+  return (
+    <section className="w-screen h-screen flex items-center justify-center">
+      <form className="w-11/12 md:w-1/2 xl:w-1/4">
+        <section className="header-section my-8">
+          <h3 className="text-3xl capitalize font-bold text-secondary">
+            Forgot Password
+          </h3>
+          <Text>Enter the email associated with this account</Text>
+        </section>
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
+        <section className="my-4 mb-5">
+          <label htmlFor="email" className="text-md block my-2">
+            Email
+          </label>
+          <Input type="email" name="email" placeholder="Enter your email" />
+        </section>
 
-export default Index;
-    
+        <section className="my-4 mb-5 w-full">
+          <Button>Submit</Button>
+        </section>
+
+        <section>
+          <Text className="inline">
+            no account?
+            <Link
+              className="capitalize text-secondary px-1"
+              href={"/auth/signup"}
+            >
+              create account
+            </Link>
+          </Text>
+        </section>
+      </form>
+    </section>
+  );
+};
+
+export default ForgotPassword;
