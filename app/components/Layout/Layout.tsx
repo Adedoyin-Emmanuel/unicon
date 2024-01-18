6e094ff7e95be6402001572b420f066c20a6a2cd
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-18 18:42:33 +0100

    Feat: Finished user events dashboard

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -6,18 +6,26 @@ interface LayoutProps {
   className?: string;
   children: React.ReactNode;
   showInput?: boolean;
+  showLogo?: boolean;
 }
 
-const Layout = ({ className, children, showInput = true }: LayoutProps) => {
+const Layout = ({
+  className,
+  children,
+  showInput = true,
+  showLogo = true,
+}: LayoutProps) => {
   return (
     <div className="w-full h-full container mx-auto">
       <section className="mx-auto w-full flex my-1 items-center justify-between md:p-3 p-0">
         <section className="w-11/12 gap-x-2 xl:w-2/6 flex items-center xl:gap-x-10 p-2">
-          <img
-            src="/assets/logo.svg"
-            alt="Unicon logo"
-            className="w-10 h-10 transform -translate-y-1 cursor-pointer"
-          />
+          {showLogo && (
+            <img
+              src="/assets/logo.svg"
+              alt="Unicon logo"
+              className="md:block hidden w-10 h-10 transform -translate-y-1 cursor-pointer"
+            />
+          )}
           {showInput && (
             <input
               type="text"
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,19 +1,100 @@
-
-"use client"
+"use client";
 import React from "react";
 import { useRouter, usePathname } from "next/navigation";
+import Layout from "@/app/components/Layout/Layout";
+import Text from "@/app/components/Text/Text";
 
+const EventDashboard = () => {
+  const pathname = usePathname();
 
-const Index = () => {
-    const pathname = usePathname();
+  return (
+    <Layout>
+      <section className="w-full">
+        <section className="header my-3">
+          <h2 className="font-bold capitalize text-2xl">event management</h2>
+          <p>
+            Effortlessly monitor your created and registered events all in one
+            place
+          </p>
+        </section>
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
+        <section className="analytics mx-auto w-full flex md:flex-row flex-col md:items-center md:justify-between gap-x-5 my-5 md:p-0 p-1">
+          <section className="w-full cursor-pointer created-events bg-primary/15 rounded-md border-[1px] border-primary p-3 flex flex-col items-center justify-center md:w-72 h-24 my-3">
+            <h1 className="text-4xl font-extrabold">0</h1>
+            <h2 className="font-medium text-[15px] capitalize py-1">
+              created events
+            </h2>
+          </section>
 
-export default Index;
-    
+          <section className="w-full cursor-pointer registered-events bg-primary/15 rounded-md border-[1px] border-primary p-3 flex flex-col items-center justify-center md:w-72 h-24 my-3">
+            <h1 className="text-4xl font-extrabold">0</h1>
+            <h2 className="font-medium text-[15px] capitalize py-1">
+              registered events
+            </h2>
+          </section>
+
+          <section className="w-full cursor-pointer created-events bg-primary rounded-md border-[1px] border-primary p-3 flex flex-col items-center justify-center md:w-72 h- my-3">
+            <h1 className="text-4xl font-extrabold my-3">
+              <svg
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="block"
+              >
+                <path
+                  d="M21.896 13.944H13.832V22.008H8.6V13.944H0.584V8.712H8.6V0.647999H13.832V8.712H21.896V13.944Z"
+                  fill="white"
+                />
+              </svg>
+            </h1>
+
+            <h2 className="font-medium text-[15px] capitalize py-1 text-white ">
+              create an event
+            </h2>
+          </section>
+        </section>
+
+        <section className="my-events">
+          <div role="tablist" className="tabs tabs-lifted w-full">
+            <input
+              type="radio"
+              name="my_tabs_2"
+              role="tab"
+              className="tab font-bold"
+              aria-label="Created Events"
+              defaultChecked
+            />
+            <div
+              role="tabpanel"
+              className="tab-content bg-base-100 border-x-base-100 rounded-box p-1 py-3 w-full"
+            >
+              <section className="events w-full flex flex-col  items-center justify-center my-4">
+                <Text>No events available</Text>
+              </section>
+            </div>
+
+            <input
+              type="radio"
+              name="my_tabs_2"
+              role="tab"
+              className="tab font-medium"
+              aria-label="Registered Events"
+            />
+            <div
+              role="tabpanel"
+              className="tab-content bg-base-100 border-x-base-100 rounded-box p-1 py-3 w-full"
+            >
+              <section className="events w-full flex flex-col  items-center justify-center my-4">
+                <Text>No events available</Text>
+              </section>
+            </div>
+          </div>
+        </section>
+      </section>
+    </Layout>
+  );
+};
+
+export default EventDashboard;
