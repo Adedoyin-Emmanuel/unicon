395f78fff8bb0d6b9bccfcdb20c307979ff7e3b9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-18 12:56:22 +0100

    Feat: Added physical reg event page

--- app/events/create/page.tsx
+++ app/events/create/page.tsx
@@ -1,19 +1,16 @@
-
-"use client"
+"use client";
 import React from "react";
 import { useRouter, usePathname } from "next/navigation";
 
+const CreateEvent = () => {
+  const pathname = usePathname();
 
-const Index = () => {
-    const pathname = usePathname();
+  return (
+    <div>
+      <h1>Index works!</h1>
+      <p>Current pathname: {pathname}</p>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
-}
-
-export default Index;
-    
+export default CreateEvent;
